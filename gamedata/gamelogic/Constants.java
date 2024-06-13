5f986499f973985c37ed897673dfc6b86ace70ab
haseemeeg <haseemeeg@gmail.com>
2024-06-13 12:20:54 +1000

    created audio and image packages in assets package

--- gamedata/gamelogic/Constants.java
+++ gamedata/gamelogic/Constants.java
@@ -1,5 +1,6 @@
 package gamedata.gamelogic;
 
 public final class Constants {
-    public static final String RESOURCES_FILEPATH = "/gamedata/resources/";
+    public static final String IMAGES_FILEPATH = "/gamedata/assets/images/";
+    public static final String AUDIO_FILEPATH = "/gamedata/assets/audio/";
 }
